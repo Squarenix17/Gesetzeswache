825dc39fb683f09721d9dc22b8a8ccf4fcb55a70
Gabriele <73937907+Squarenix17@users.noreply.github.com>
2026-07-29 09:55:43 +0200

    chore: untrack ECC .cursor harness; keep Go rules and agents

--- .cursor/hooks/pre-compact.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-const { readStdin, runExistingHook, transformToLegacyHook } = require('./adapter');
-readStdin().then(raw => {
-  const cursorInput = JSON.parse(raw || '{}');
-  runExistingHook('pre-compact.js', transformToLegacyHook(cursorInput));
-  process.stdout.write(raw);
-}).catch(() => process.exit(0));
